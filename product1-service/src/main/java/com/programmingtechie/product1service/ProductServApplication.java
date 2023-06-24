3c216ede879f9e19968da23f04f4d12dc88a3116
artem.hudyakov2000@gmail.com <artem.hudyakov2000@gmail.com>
2023-06-24 18:44:48 +0300

    Added discovery server and Netflix Eureka

--- product1-service/src/main/java/com/programmingtechie/product1service/ProductServApplication.java
+++ product1-service/src/main/java/com/programmingtechie/product1service/ProductServApplication.java
@@ -2,9 +2,11 @@ package com.programmingtechie.product1service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 
 @SpringBootApplication()
+@EnableEurekaClient
 public class ProductServApplication {
 
 	public static void main(String[] args) {
